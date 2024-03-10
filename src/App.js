fb8e19df06e66c2d559fb13b57c36fdb2a8aab16
Birkan <birkanyksl613@gmail.com>
2024-03-10 19:40:54 +0300

    react async

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
+import { useEffect, useState } from "react";
+import Kisi from "./bilesenler/Kisi";
+import KisiSkelaton from "./bilesenler/KisiSkelaton";
+
 function App() {
-  return <div>app</div>;
+  const [veri, veriGuncelle] = useState([]);
+  const [yukleniyor, yukleniyorGuncelle] = useState(true);
+
+  useEffect(() => {
+    async function rehberCek() {
+      const rehberJSON = await localStorage.getItem("rehberJSON");
+      const rehberVeri = JSON.parse(rehberJSON);
+
+      veriGuncelle(rehberVeri);
+      yukleniyorGuncelle(false);
+    }
+
+    setTimeout(rehberCek, 2000);
+  }, []);
+
+  //console.log(veri);
+
+  return (
+    <>
+      <section className="container mt-5">
+        <h1>Rehber</h1>
+
+        <div className="row">
+          {yukleniyor && <KisiSkelaton />}
+          {veri.map((eleman) => (
+            <Kisi key={eleman.id} ad={eleman.ad} tel={eleman.tel} />
+          ))}
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default App;
